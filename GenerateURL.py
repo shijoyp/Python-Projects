5c7b850c4f5aec9d3ed1504eedb2e932157c5e17
shijoyp <shijoy22@gmail.com>
2018-04-01 00:34:37 +0800

    Version 2

--- GenerateURL.py
+++ GenerateURL.py
@@ -7,17 +7,5 @@ STOCKNAME = []
 for i in Alp:
     URLS.append("http://www.moneycontrol.com/india/stockpricequote/" + i)
 
-
 for url in URLS:
-    res = requests.get(url)
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-
-    for sname in soup.find_all("a",{"class" : "bl_12"}):
-        STOCKNAME.append(sname.get("title"))
-
-
-
-print len(STOCKNAME)
-
-for stock in STOCKNAME:
-    print stock
+    print url
--- Moneycontrolscraping.py
+++ Moneycontrolscraping.py
@@ -1,10 +1,23 @@
+import string
 import requests, bs4
 
-res = requests.get('http://www.moneycontrol.com/india/stockpricequote/A')
+Alp = string.ascii_uppercase[:]
+URLS = []
+STOCKNAME = []
+for i in Alp:
+    URLS.append("http://www.moneycontrol.com/india/stockpricequote/" + i)
 
-soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-for sname in soup.find_all("a",{"class" : "bl_12"}):
-    print(sname.get("title"))
+for url in URLS:
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+    for sname in soup.find_all("a",{"class" : "bl_12"}):
+        STOCKNAME.append(sname.get("title"))
 
 
+
+print len(STOCKNAME)
+
+for stock in STOCKNAME:
+    print stock
