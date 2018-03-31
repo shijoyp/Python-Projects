bb5bcbdf541d95b87c0892127d1fa35a98a642f7
shijoyp <shijoy22@gmail.com>
2018-03-31 23:58:37 +0800

    Version 1

---
+++ Moneycontrolscraping.py
@@ -0,0 +1,10 @@
+import requests, bs4
+
+res = requests.get('http://www.moneycontrol.com/india/stockpricequote/?classic=true')
+
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+for sname in soup.find_all("a",{"class" : "bl_12"}):
+    print(sname.get("title"))
+
+
